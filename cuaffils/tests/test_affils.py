67e4ab28a5c0e9f6090807fcebbb3bc3792e3311
Anders Pearson <anders@columbia.edu>
2014-09-05 16:32:50 +0200

    add a second test case with different values for every field

--- cuaffils/tests/test_affils.py
+++ cuaffils/tests/test_affils.py
@@ -4,9 +4,6 @@ from cuaffils import (
     generate_pamacea_string,
     generate_course_directory_string)
 
-SIMPLE_TEST_FIELDS = dict(
-)
-
 TEST_CASES = [
     dict(
         fields=dict(
@@ -19,7 +16,22 @@ TEST_CASES = [
             ),
         wind='t3.y2014.s001.cN6610.NURS.st.course:columbia.edu',
         pamacea='CUcourse_NURSN6610_001_2014_3',
-        course_dir='20143NURS6610N001'),
+        course_dir='20143NURS6610N001'
+    ),
+
+    dict(
+        fields=dict(
+            term='spring',
+            year=2010,
+            section_number='010',
+            course_prefix_letter='F',
+            course_number=1234,
+            department_id='EENG'
+            ),
+        wind='t1.y2010.s010.cF1234.EENG.st.course:columbia.edu',
+        pamacea='CUcourse_EENGF1234_010_2010_1',
+        course_dir='20101EENG1234F010'
+    ),
 ]
 
 class GenerateWindStringTest(unittest.TestCase):
