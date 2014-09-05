cbd97bbf6f548bfb5d55b50e89b60c2221233889
Anders Pearson <anders@columbia.edu>
2014-09-05 16:28:47 +0200

    set up to handle more test cases

--- cuaffils/tests/test_affils.py
+++ cuaffils/tests/test_affils.py
@@ -5,27 +5,42 @@ from cuaffils import (
     generate_course_directory_string)
 
 SIMPLE_TEST_FIELDS = dict(
-    term='fall',
-    year=2014,
-    section_number='001',
-    course_prefix_letter='N',
-    course_number=6610,
-    department_id='NURS'
 )
+
+TEST_CASES = [
+    dict(
+        fields=dict(
+            term='fall',
+            year=2014,
+            section_number='001',
+            course_prefix_letter='N',
+            course_number=6610,
+            department_id='NURS'
+            ),
+        wind='t3.y2014.s001.cN6610.NURS.st.course:columbia.edu',
+        pamacea='CUcourse_NURSN6610_001_2014_3',
+        course_dir='20143NURS6610N001'),
+]
 
 class GenerateWindStringTest(unittest.TestCase):
     def test_basic(self):
-        s = 't3.y2014.s001.cN6610.NURS.st.course:columbia.edu'
-        self.assertEqual(generate_wind_string(**SIMPLE_TEST_FIELDS), s)
+        for c in TEST_CASES:
+            fields = c['fields']
+            s = c['wind']
+            self.assertEqual(generate_wind_string(**fields), s)
 
 
 class GeneratePamaceaStringTest(unittest.TestCase):
     def test_basic(self):
-        s = 'CUcourse_NURSN6610_001_2014_3'
-        self.assertEqual(generate_pamacea_string(**SIMPLE_TEST_FIELDS), s)
+        for c in TEST_CASES:
+            fields = c['fields']
+            s = c['pamacea']
+            self.assertEqual(generate_pamacea_string(**fields), s)
 
 
 class GenerateCourseDirectoryStringTest(unittest.TestCase):
     def test_basic(self):
-        s = '20143NURS6610N001'
-        self.assertEqual(generate_course_directory_string(**SIMPLE_TEST_FIELDS), s)
+        for c in TEST_CASES:
+            fields = c['fields']
+            s = c['course_dir']
+            self.assertEqual(generate_course_directory_string(**fields), s)
